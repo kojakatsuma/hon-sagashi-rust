9ffcf40a91fcef9b0754337f1970af02580575a6
kojakatsuma <katsuma.koja@ubiregi.com>
2021-02-15 14:42:59 +0900

    use fantoccini

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
-use headless_chrome::{protocol::page::ScreenshotFormat, Browser};
-use std::fs;
+use fantoccini::ClientBuilder;
 
-fn browse_wiki() -> Result<(), failure::Error> {
-    let browser = Browser::default()?;
-    let tab = browser.wait_for_initial_tab()?;
-    tab.navigate_to("https://www.wikipedia.org")?;
-    tab.wait_for_element("input#searchInput")?.click()?;
-    tab.type_str("WebKit")?.press_key("Enter")?;
-    tab.wait_for_element("#firstHeading")?;
-    assert!(tab.get_url().ends_with("WebKit"));
-    let _jpg_data = tab.capture_screenshot(ScreenshotFormat::JPEG(Some(75)), None, true)?;
-    fs::write("screenshot.jpg", &_jpg_data)?;
-    let _png_data = tab
-        .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-        .capture_screenshot(ScreenshotFormat::PNG)?;
-    fs::write("screenshot.png", &_png_data)?;
+async fn get_items() -> Result<(), fantoccini::error::CmdError> {
+    // let cap = webdriver::capabilities::Capabilities::new();
+    let mut c = ClientBuilder::native()
+        // .capabilities()
+        .connect("http://localhost:9515")
+        .await
+        .expect("failed to connect to WebDriver");
+    c.goto("https://www.amazon.co.jp/hz/wishlist/ls/1LT97CIJHMD3V?viewType=grid")
+        .await?;
+    const SCROLL: &str = r#"
+        let end = document.getElementById('endOfListMarker')
+        let count = 1;
+        let id = setInterval(()=> {
+            window.scrollTo(0,window.outerHeight*count)
+            end = document.getElementById('endOfListMarker')
+            count ++   
+        },500)
+    "#;
+    c.execute(SCROLL, vec![]).await?;
+    const GET_ITEMS: &str = r#"
+        const [callback] = arguments;
+        let end = document.getElementById('endOfListMarker')
+        let id = setInterval(() => {
+            if(end){
+                const titles = []
+                document.querySelectorAll('div.a-section.a-spacing-none.wl-grid-item-content.wl-grid-item-flex-container > div > a')
+                    .forEach(({ title, href }) => title && href && titles.push({ title, amazonUrl: href }));
+                callback(titles)
+            }
+            end = document.getElementById('endOfListMarker')
+        },500)
+    "#;
+    let items = c.execute_async(GET_ITEMS, vec![]).await?;
+    let x = items.as_array().unwrap();
+    print!("{}", x.len());
+    for i in x {
+        println!("{:?}", i);
+    }
+    c.close().await?;
     Ok(())
 }
 
-fn main() {
-   assert!(browse_wiki().is_ok())
+#[tokio::main]
+async fn main() -> Result<(), fantoccini::error::CmdError> {
+    get_items().await
 }
