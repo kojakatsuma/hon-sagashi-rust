5195a9e0fb60efe560c0427a8b8520cee62f5192
kojakatsuma <katsuma.koja@ubiregi.com>
2021-02-15 23:03:18 +0900

    get items

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use fantoccini::ClientBuilder;
+use fantoccini::{ClientBuilder, Locator};
+
+
+fn create_cap() -> serde_json::map::Map<String, serde_json::Value> {
+    let mut caps = serde_json::map::Map::new();
+    let opts = serde_json::json!({"args":["--headless"]});
+    caps.insert("goog:chromeOptions".to_string(), opts.clone());
+    caps
+}
 
 async fn get_items() -> Result<(), fantoccini::error::CmdError> {
-    // let cap = webdriver::capabilities::Capabilities::new();
+    let caps = create_cap();
     let mut c = ClientBuilder::native()
-        // .capabilities()
+        .capabilities(caps)
         .connect("http://localhost:9515")
         .await
         .expect("failed to connect to WebDriver");
@@ -11,26 +19,26 @@ async fn get_items() -> Result<(), fantoccini::error::CmdError> {
         .await?;
     const SCROLL: &str = r#"
         let end = document.getElementById('endOfListMarker')
-        let count = 1;
+        let count = 1
         let id = setInterval(()=> {
-            window.scrollTo(0,window.outerHeight*count)
+            window.scrollTo(0,10000*count)
             end = document.getElementById('endOfListMarker')
-            count ++   
+            console.log(document.querySelectorAll('div.a-section.a-spacing-none.wl-grid-item-content.wl-grid-item-flex-container > div > a').length)
+            if(end){
+                console.log("------------scroll end------------")
+                clearInterval(id)
+            }
+            count ++
         },500)
     "#;
     c.execute(SCROLL, vec![]).await?;
+    c.wait_for_find(Locator::Id("endOfListMarker")).await?;
     const GET_ITEMS: &str = r#"
         const [callback] = arguments;
-        let end = document.getElementById('endOfListMarker')
-        let id = setInterval(() => {
-            if(end){
-                const titles = []
-                document.querySelectorAll('div.a-section.a-spacing-none.wl-grid-item-content.wl-grid-item-flex-container > div > a')
-                    .forEach(({ title, href }) => title && href && titles.push({ title, amazonUrl: href }));
-                callback(titles)
-            }
-            end = document.getElementById('endOfListMarker')
-        },500)
+        const titles = []
+        document.querySelectorAll('div.a-section.a-spacing-none.wl-grid-item-content.wl-grid-item-flex-container > div > a')
+            .forEach(({ title, href }) => title && href && titles.push({ title, amazonUrl: href }));
+        callback(titles)
     "#;
     let items = c.execute_async(GET_ITEMS, vec![]).await?;
     let x = items.as_array().unwrap();
